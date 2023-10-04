acbf6f5ba87c1d8c5e68b23d30221ef766f4a67b
Axelbresa <bresanovichaxel43@gmail.com>
2023-10-04 12:12:17 -0300

    primer commit, creacion de react con create vite y se agrego el readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# instalación:
+
+# npm install
+
+# Configurar variables de entorno:
+
+Ejecución en desarrollo:
+
+# npm run dev
+
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum fugit veritatis eius veniam exercitationem. Deserunt maxime tempore delectus. Deserunt inventore rerum laboriosam soluta quidem. Placeat ipsa excepturi quibusdam officia ad.</h1>
+    </>
+  )
+}
+
+export default App
